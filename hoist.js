d0c10f5d27e60bb169bb8aab374c1412fe2005f3
shoonia <alexanderz@wix.com>
2021-01-03 23:35:13 +0200

    Exclude unused files

--- hoist.js
+++ hoist.js
@@ -5,11 +5,22 @@
  */
 
 const { resolve } = require('path');
-const { copy } = require('fs-extra');
+const { emptyDir, copy, outputJSON } = require('fs-extra');
+const pkg = require('@hail2u/css-mqpacker/package.json');
 
-const fromPath = resolve(__dirname, 'node_modules/@hail2u/css-mqpacker');
-const toPath = resolve(__dirname, 'css-mqpacker');
+const fromDir = resolve(__dirname, 'node_modules/@hail2u/css-mqpacker');
+const toDir = resolve(__dirname, 'css-mqpacker');
+
+const toPkg = {
+  name: pkg.name,
+  version: pkg.version,
+  author: pkg.author,
+  homepage: pkg.homepage,
+  license: pkg.license,
+};
 
 (async () => {
-  await copy(fromPath, toPath);
+  await emptyDir(toDir);
+  await copy(resolve(fromDir, 'index.js'), resolve(toDir, 'index.js'));
+  await outputJSON(resolve(toDir, 'package.json'), toPkg, { spaces: 2 });
 })();

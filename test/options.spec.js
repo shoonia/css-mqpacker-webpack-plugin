0e3d7fa89a1548efc881d64d8c195a170e4269e0
shoonia <alexanderz@wix.com>
2022-01-05 07:06:43 +0200

    Add tests

--- test/options.spec.js
+++ test/options.spec.js
@@ -4,6 +4,8 @@ const { validate } = require('schema-utils');
 
 const schema = require('../src/options.json');
 
+const invalidList = [1, 0, '', null, {}, x => x, [null]];
+
 const check = (options) => validate(schema, options, {
   name: "CssMqpackerPlugin",
   baseDataPath: "options",
@@ -13,20 +15,79 @@ test('empty options', () => {
   is(check({}), undefined);
 });
 
+test('invalid options', () => {
+  throws(() => check(x => x));
+});
+
+// test:
+
+test('valid `test` option when it is a string', () => {
+  is(check({ test: '.css' }), undefined);
+  is(check({ test: ['.css'] }), undefined);
+});
+
+test('valid `test` option when it is a RegExp', () => {
+  is(check({ test: /\.css/ }), undefined);
+  is(check({ test: [/\.css/] }), undefined);
+});
+
+test('invalid `test` option', () => {
+  invalidList.forEach((test) => {
+    throws(() => check({ test }));
+  });
+});
+
+// include:
+
+test('valid `include` option when it is a string', () => {
+  is(check({ include: '.css' }), undefined);
+  is(check({ include: ['.css'] }), undefined);
+});
+
+test('valid `include` option when it is a RegExp', () => {
+  is(check({ include: /\.css/ }), undefined);
+  is(check({ include: [/\.css/] }), undefined);
+});
+
+test('invalid `include` option', () => {
+  invalidList.forEach((include) => {
+    throws(() => check({ include }));
+  });
+});
+
+// exclude:
+
+test('valid `exclude` option when it is a string', () => {
+  is(check({ exclude: '.css' }), undefined);
+  is(check({ exclude: ['.css'] }), undefined);
+});
+
+test('valid `exclude` option when it is a RegExp', () => {
+  is(check({ exclude: /\.css/ }), undefined);
+  is(check({ exclude: [/\.css/] }), undefined);
+});
+
+test('invalid `exclude` option', () => {
+  invalidList.forEach((exclude) => {
+    throws(() => check({ exclude }));
+  });
+});
+
+// sort:
+
 test('valid `sort` option when it is a boolean', () => {
   is(check({ sort: true }), undefined);
   is(check({ sort: false }), undefined);
 });
 
-test('valid `sort` option when it is function', () => {
+test('valid `sort` option when it is a function', () => {
   is(check({ sort: x => x }), undefined);
 })
 
 test('invalid `sort` option', () => {
-  throws(() => check({ sort: 1 }));
-  throws(() => check({ sort: 0 }));
-  throws(() => check({ sort: 'some' }));
-  throws(() => check({ sort: null }));
+  [1, 0, '', 'some', null].forEach((sort) => {
+    throws(() => check({ sort }));
+  });
 });
 
 test.run();

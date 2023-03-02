391b5037eb8e6c80f63ecc881b188b05d0b00cf6
shoonia <alexanderz@wix.com>
2023-03-02 22:17:39 +0200

    Update types

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,12 +1,13 @@
 interface Options {
-  test?: RegExp
-  include?: RegExp
-  exclude?: RegExp
+  test?: string | RegExp | (string | RegExp)[]
+  include?: string | RegExp | (string | RegExp)[]
+  exclude?: string | RegExp | (string | RegExp)[]
   sort?: boolean
 }
 
 declare class CssMqpackerPlugin {
   constructor(options?: Options)
+  apply(compiler: import("webpack").Compiler): void;
 }
 
 export = CssMqpackerPlugin

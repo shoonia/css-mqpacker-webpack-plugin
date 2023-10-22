c4a080e48c2ecb4b1240612140db8d2ffc3d30d9
shoonia <alexanderz@wix.com>
2023-10-22 20:25:20 +0300

    Add commets

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,7 +1,19 @@
 interface Options {
+  /**
+   * Include all modules that pass test assertion
+   */
   test?: string | RegExp | (string | RegExp)[]
+  /**
+   * Include all modules matching any of these conditions
+   */
   include?: string | RegExp | (string | RegExp)[]
+  /**
+   * Exclude all modules matching any of these conditions
+   */
   exclude?: string | RegExp | (string | RegExp)[]
+  /**
+   * Sort media queries
+   */
   sort?: boolean | ((a: string, b: string) => number)
 }
 

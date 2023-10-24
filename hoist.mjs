7cccb1b02f0eb35debd381e16b8acee321a14907
shoonia <alexanderz@wix.com>
2023-10-24 21:21:54 +0300

    Use fs

--- hoist.mjs
+++ hoist.mjs
@@ -5,23 +5,29 @@
  */
 
 import { resolve } from 'node:path';
-import fse from 'fs-extra';
+import { existsSync } from 'node:fs';
+import { rm, mkdir, copyFile, writeFile } from 'node:fs/promises';
 import pkg from '@hail2u/css-mqpacker/package.json' assert { type: 'json' };
 
 const fromDir = './node_modules/@hail2u/css-mqpacker';
 const toDir = './css-mqpacker';
 
-const packageJson = {
+const packageJson = JSON.stringify({
   name: pkg.name,
   version: pkg.version,
   author: pkg.author,
   homepage: pkg.homepage,
   license: pkg.license,
   private: true,
-};
+}, null, 2);
 
-await fse.emptyDir(toDir);
+if (existsSync(toDir)) {
+  await rm(toDir, { recursive: true });
+}
+
+await mkdir(toDir);
+
 await Promise.all([
-  fse.copy(resolve(fromDir, 'index.js'), resolve(toDir, 'index.js')),
-  fse.outputJSON(resolve(toDir, 'package.json'), packageJson, { spaces: 2 }),
+  copyFile(resolve(fromDir, 'index.js'), resolve(toDir, 'index.js')),
+  writeFile(resolve(toDir, 'package.json'), packageJson + '\n'),
 ]);
